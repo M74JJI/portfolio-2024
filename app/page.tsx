8155f864979a5c0b6cc696db047deff5406d5331
M74JJI <pytaek1@gmail.com>
2024-02-27 20:54:26 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import WaterWaveWrapper from "@/components/visualEffects/water-wave-wrapper";
+import AboutSection from "@/sections/about";
+import ContactSection from "@/sections/contact";
+import FeaturedSection from "@/sections/featured";
+import LandingSection from "@/sections/landing";
+
+export default function Home() {
+  return (
+    <WaterWaveWrapper
+      imageUrl=""
+      dropRadius="3"
+      perturbance="3"
+      resolution="2048"
+    >
+      {() => (
+        <div className="pb-8">
+          <LandingSection />
+          <FeaturedSection />
+          <AboutSection />
+          <ContactSection />
+        </div>
+      )}
+    </WaterWaveWrapper>
+  );
+}
